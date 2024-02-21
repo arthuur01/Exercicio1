b4899da4497d982f7e7665d953d410122dea9668
Arthur <Arthur@DESKTOP-8ENFDJL>
2024-02-21 12:14:26 -0300

    Commit Inicial

---
+++ exercicio1/src/exercicio1/soma.java
@@ -0,0 +1,17 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class soma {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+				
+		int x = sc.nextInt();
+		int y = sc.nextInt();	
+		sc.close();
+		System.out.println(x + y);
+		
+	}
+	
+	
+}
